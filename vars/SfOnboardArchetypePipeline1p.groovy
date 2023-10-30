ddc1338107eabcdeb21d295114371a2773ecce60
vinaykontham <148061393+vinaykontham@users.noreply.github.com>
2023-10-30 21:18:55 +0530

    Update SfOnboardArchetypePipeline1p.groovy

--- vars/SfOnboardArchetypePipeline1p.groovy
+++ vars/SfOnboardArchetypePipeline1p.groovy
@@ -72,13 +72,13 @@ def call() {
         echo "Maven Home is ${mvnHome}"
         def mvnExecutable = "${mvnHome}/opt/maven"
 
-     def exampleApi = "mvn archetype:generate " +
-            "-DarchetypeGroupId=com.sidgs.sharedflow.archetype " +
-            "-DarchetypeArtifactId=sf " +
+   def exampleApi = "mvn archetype:generate " +
+            "-DarchetypeGroupId=com.sidgs.sharedflow.commons " +
+            "-DarchetypeArtifactId=sharedflows-parent-pom " +
             "-DarchetypeVersion=1.0.0-SNAPSHOT " +
-            "-DgroupId=com-sidgs-sharedflow-archetype " +
+            "-DgroupId=com-sidgs-sharedflow-commons " +
             "-DartifactId=${params.sfName} " +
-            "-Dpackage=sidgs-sf" +
+            "-Dpackage=sidgs-sharedflows-parent-pom" +
             "-DsfName=${params.sfName} " +
             "-DinteractiveMode=false"
         
