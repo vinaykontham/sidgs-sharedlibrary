c064bcaccd5dcb28b3ab4afc92a638799e984cb0
vinaykontham <148061393+vinaykontham@users.noreply.github.com>
2023-11-04 10:07:25 +0530

    Update branchManagement_sf.groovy

--- vars/branchManagement_sf.groovy
+++ vars/branchManagement_sf.groovy
@@ -24,7 +24,7 @@ This pipeline is used for handling branch management on the repos
  */
 def call(String operation,String repoProjectName) {
 
-    node {
+    node ('master'){
         deleteDir()
         def shell = new shell()
         try {
